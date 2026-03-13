eb6e98168d6c47d433a138900157195ade572ecf
saavedras22-sys <saavedras22@itisvinci.com>
2026-03-13 18:01:03 +0100

    Movement update 1.12

--- Assets/Scripts/MoveAula1.cs
+++ Assets/Scripts/MoveAula1.cs
@@ -2,34 +2,50 @@ using UnityEngine;
 
 public class MoveAula1 : MonoBehaviour
 {
-    public string folderPath = "Aula1/aula_";
-    private Material[] skyboxes;
-    private int currentIndex = 0;
-
-    void Start()
-    {
-        skyboxes = Resources.LoadAll<Material>("Aula1");
-        
-        if (skyboxes.Length > 0)
-            RenderSettings.skybox = skyboxes[currentIndex];
-    }
-
-    public void NextSkybox()
-    {
-        currentIndex = (currentIndex + 1) % skyboxes.Length;
-        UpdateSkybox();
-    }
-
-    public void PreviousSkybox()
-    {
-        currentIndex--;
-        if (currentIndex < 0) currentIndex = skyboxes.Length - 1;
-        UpdateSkybox();
-    }
-
-    void UpdateSkybox()
-    {
-        RenderSettings.skybox = skyboxes[currentIndex];
+    public string folderPath = "Aula1/aula_"; 
+    private Material[] skyboxes; 
+    private int currentIndex = 0; 
+    private GameObject right; 
+    private GameObject left; 
+    private GameObject father;
+    void Start() { 
+        skyboxes = Resources.LoadAll<Material>("Aula1"); 
+        if (skyboxes.Length > 0) 
+            RenderSettings.skybox = skyboxes[currentIndex]; 
+        father = GameObject.Find("Right_Left_Buttons");
+        right = father.transform.GetChild (0).gameObject;
+        left = father.transform.GetChild (1).gameObject;
+        if(currentIndex == 0) { 
+            left.SetActive(false); 
+        } 
+    } 
+    public void NextSkybox() {
+        currentIndex = (currentIndex + 1) % skyboxes.Length; 
+        UpdateSkybox(); 
+    } 
+    public void PreviousSkybox() { 
+        currentIndex--; 
+        if (currentIndex <= 0) { 
+            left.SetActive(false); 
+        } 
+        UpdateSkybox(); 
+    } 
+    void UpdateSkybox() { 
+        right = father.transform.GetChild (0).gameObject;
+        left = father.transform.GetChild (1).gameObject;
+        RenderSettings.skybox = skyboxes[currentIndex]; 
         DynamicGI.UpdateEnvironment(); 
+        if(currentIndex == 1) { 
+            right.SetActive(true); 
+            left.SetActive(true); 
+        } else if(currentIndex < 1)
+        {
+            right.SetActive(true);
+            left.SetActive(false);
+        } else
+        {
+            right.SetActive(false);
+            left.SetActive(true);
+        }
     }
 }
