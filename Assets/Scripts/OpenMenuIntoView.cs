ad6a49637715dbf8bd0a2b0b31bed96f5f673d33
redeghieriv22-itis <redeghieriv22@itisvinci.com>
2026-03-16 12:09:34 +0100

    hand menu script

---
+++ Assets/Scripts/OpenMenuIntoView.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class OpenMenuIntoView : MonoBehaviour
+{
+    public float distance = 0.6f; // Distance from the camera
+    private bool menuPresent = false;
+    private GameObject lastMenuOpened;
+    public void OpenMenu(GameObject menuParent)
+    {
+        if (menuPresent)
+        {
+            Close(lastMenuOpened);
+        }
+        GameObject menu = menuParent.transform.GetChild (0).gameObject;
+        menu.SetActive(true);
+        // Get the camera's position and forward direction
+        Transform camTransform = Camera.main.transform;
+        
+        // Calculate the target position in front of the camera (ignores Y)
+        Vector3 forwardFixed = camTransform.forward;
+        forwardFixed.y = 0; 
+        Vector3 targetPosition = camTransform.position + (forwardFixed.normalized * distance);
+
+        // Instant snap to view
+        menu.transform.position = targetPosition;
+
+        menu.transform.LookAt(camTransform);
+        menu.transform.Rotate(0, 180, 0); // LookAt points at the back of the menu
+
+        menuPresent = true;
+        lastMenuOpened = menu;
+    }
+
+    public void CloseMenu(GameObject menuParent)
+    {
+        GameObject menu = menuParent.transform.GetChild (0).gameObject;
+        Close(menu);
+    }
+
+    private void Close(GameObject menu)
+    {
+        menu.SetActive(false);
+        menuPresent = false;
+    }
+}
