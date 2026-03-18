a13ceb22478fd46c335c177935d107faf3115792
saavedras22-sys <saavedras22@itisvinci.com>
2026-03-18 08:56:41 +0100

    build fixes

--- Assets/Scripts/MoveAula3.cs
+++ Assets/Scripts/MoveAula3.cs
@@ -18,7 +18,6 @@ public class MoveAula3 : MonoBehaviour
         father = GameObject.Find("Right_Left_Buttons");
         buttons_canvas = GameObject.Find("Buttons2");
         //camera = GameObject.Find("XR Origin (XR Rig)");
-        camera = GameObject.Find("Main Camera");
         right = father.transform.GetChild (0).gameObject;
         left = father.transform.GetChild (1).gameObject;
         if(currentIndex == 0) { 
@@ -53,64 +52,5 @@ public class MoveAula3 : MonoBehaviour
             right.SetActive(false);
             left.SetActive(true);
         }
-        //Reposition();
-    }
-    void Reposition()
-    {
-        switch(currentIndex)
-        {
-            case 0:
-                Reposition_Sky1();
-                break;
-            case 1:
-                Reposition_Sky2();
-                break;
-            case 2:
-                Reposition_Sky3();
-                break;
-            case 3:
-                Reposition_Sky4();
-                break;
-            case 4:
-                Reposition_Sky5();
-                break;
-        }
-    }
-    void Reposition_Sky1()
-    {
-        Transform targetTransform = GameObject.Find("target_1").transform;
-        camera.transform.LookAt(targetTransform);
-        buttons_canvas.transform.LookAt(targetTransform);
-    }
-    void Reposition_Sky2()
-    {
-        buttons_canvas.transform.eulerAngles = new Vector3(
-            buttons_canvas.transform.eulerAngles.x,
-            buttons_canvas.transform.eulerAngles.y,
-            buttons_canvas.transform.eulerAngles.z
-        );
-        left.transform.eulerAngles = new Vector3(
-            left.transform.eulerAngles.x,
-            left.transform.eulerAngles.y,
-            180.195f
-        );
-        camera.transform.eulerAngles = new Vector3(0,180,0);
-    }
-    void Reposition_Sky3()
-    {
-        buttons_canvas.transform.eulerAngles = new Vector3(90,183.9254f,0);
-        left.transform.eulerAngles = new Vector3(-2.14062129e-05f,1.64829362e-05f,0.889403045f);
-        camera.transform.eulerAngles = new Vector3(0,185,0);
-    }
-    void Reposition_Sky4()
-    {
-        buttons_canvas.transform.eulerAngles = new Vector3(90,94.8505325f,0);
-        left.transform.eulerAngles = new Vector3(7.01546651e-06f,1.26773384e-05f,32.0809212f);
-        camera.transform.eulerAngles = new Vector3(0,90,0);
-    }
-    void Reposition_Sky5()
-    {
-        buttons_canvas.transform.eulerAngles = new Vector3(90,262.925659f,0);
-        camera.transform.eulerAngles = new Vector3(0,84.8191147f,0);
     }
 }
--- Assets/Scripts/SpawnInView.cs
+++ Assets/Scripts/SpawnInView.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class SpawnInView : MonoBehaviour
 {
     public float distance = 2; // Distance from the camera
+    private bool positioned = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Get the camera's position and forward direction
+        /*// Get the camera's position and forward direction
         Transform camTransform = Camera.main.transform;
         
         // Calculate the target position in front of the camera (ignores Y)
@@ -18,12 +19,28 @@ public class SpawnInView : MonoBehaviour
         transform.position = targetPosition;
 
         transform.LookAt(camTransform);
-        transform.Rotate(0, 180, 0); // LookAt points at the back of the menu
+        transform.Rotate(0, 180, 0); // LookAt points at the back of the menu*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!positioned)
+        {
+            // Get the camera's position and forward direction
+            Transform camTransform = Camera.main.transform;
+            
+            // Calculate the target position in front of the camera (ignores Y)
+            Vector3 forwardFixed = camTransform.forward;
+            forwardFixed.y = 0; 
+            Vector3 targetPosition = camTransform.position + (forwardFixed.normalized * distance);
+
+            // Instant snap to view
+            transform.position = targetPosition;
+
+            transform.LookAt(camTransform);
+            transform.Rotate(0, 180, 0); // LookAt points at the back of the menu
+            positioned = true;
+        }
     }
 }
